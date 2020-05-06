65a3e6d5327507804bcd0ae402c2846c06c027af
Marcos Vinnicius Martins <gronytzki@gmail.com>
2020-05-06 15:52:21 -0300

    Enviando o projeto

---
+++ src/test/java/com/meudominio/FarmaciaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meudominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
