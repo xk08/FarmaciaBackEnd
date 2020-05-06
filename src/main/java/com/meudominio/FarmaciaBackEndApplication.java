1ecc79968a50dfdede34305c6b46dae6e9710d5a
Marcos Vinnicius Martins <gronytzki@gmail.com>
2020-05-06 16:14:21 -0300

    Teste 02: Exibindo em tela

--- src/main/java/com/meudominio/FarmaciaBackEndApplication.java
+++ src/main/java/com/meudominio/FarmaciaBackEndApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @RestController
@@ -13,6 +14,11 @@ public class FarmaciaBackEndApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FarmaciaBackEndApplication.class, args);
 	}
+	
+	 @RequestMapping("/")
+	    public String home(){
+	        return "Hello Everybody";
+	    }
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
